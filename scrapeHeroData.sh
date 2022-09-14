f863b04d6547cec7b7fe5afb8431ee47fb657ae8
Hrusikesh Panda <mrchief@users.noreply.github.com>
2022-09-14 16:34:17 -0400

    Create scrapeHeroData.sh

---
+++ scrapeHeroData.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##### PRE REQUISITES
+## Install shot-scraper and playwright
+## Run these commands in order
+
+# pip install shot-scraper
+# playwright install
+
+# modify this URL for the hero you're scraping for
+HERO=Grace
+URL=https://empiresandpuzzles.fandom.com/wiki/$HERO
+
+shot-scraper javascript $URL "({
+  name: document.querySelector('h2[data-source=title1]').innerText,
+  class: document.querySelector('div[data-source=class] a:nth-of-type(2)').innerText,
+  source: '<TODO>',
+  speed: document.querySelector('div[data-source=mana_speed] div.pi-data-value').innerText,
+  power: Number(document.querySelector('td[data-source=power]').innerText),
+  attack: Number(document.querySelector('td[data-source=attack]').innerText),
+  defense: Number(document.querySelector('td[data-source=defense]').innerText),
+  health: Number(document.querySelector('td[data-source=health]').innerText),
+  skill: document.querySelector('div[data-source=special_name] div.pi-data-value').innerText,
+  effects: Array.from(document.querySelectorAll('div[data-source^=effect] div')).map((n) => n.innerText.split('\n')[0]),
+  types: ['<TODO>'],
+  passives: document
+    .querySelector('div[data-source=resist] div')
+    .innerText.split('\n')
+    .filter((n) => n.trim() && n !== '\n')
+    .map((n) => n.trim()),
+  image: '<TODO>',
+  family: document.querySelector('div[data-source=family] a:nth-of-type(2)').innerText,
+})" | yq -P '.' > $HERO.yaml
